919018989b361851fd71eb126fa3e2e0ee5c1051
Transport Admin <admin@transport-analitika.local>
2026-08-31 15:33:30 +0200

    chore: cleanup read_tab1 temporary files

--- read_tab1_cards.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-const html = fs.readFileSync('public/v1.html', 'utf8');
-const tab1Idx = html.indexOf('id="tabContent1"');
-const tab2Idx = html.indexOf('id="tabContent2"');
-console.log(html.substring(tab1Idx, tab1Idx + 2500));
--- read_tab1_fn.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-const html = fs.readFileSync('public/v1.html', 'utf8');
-const fnIdx = html.indexOf('function updateKPICardsTab1');
-console.log(html.substring(fnIdx, fnIdx + 1500));
